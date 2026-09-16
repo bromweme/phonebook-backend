704b2ba54fd1b13987e5951e33f8c81cdac2f18f
Michael <bromweme@gmail.com>
2026-09-16 14:47:21 -0400

    uibuild

--- index.js
+++ index.js
@@ -46,11 +46,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 app.post('/api/persons', (request, response, next) => {
     const { name, number } = request.body
-    if(!name || !number) {
-        return response.status(400).json({
-            error: 'The name or number is missing'
-        })
-    }
     Person.find({name : name})
     .then(person => {
         if(person.length) {
@@ -67,6 +62,7 @@ app.post('/api/persons', (request, response, next) => {
             person.save().then(savedPerson => {
                 return response.json(savedPerson)
             })
+            .catch(error => next(error))
         }
     })
     .catch(error => next(error))
@@ -99,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   } 
 
   next(error)
